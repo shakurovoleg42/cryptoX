ab2b20790f12e23b08335c49faf03cec99cdccfd
Oleg Shakurov <thorsonodin2003@gmail.com>
2025-03-31 09:44:33 +0500

    implement fxi with build

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import styles from "./Header.module.scss";
+
+const Header = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setSearchQuery(searchTerm);
+  };
+
+  return (
+    <div className={styles.header}>
+      <form className={styles.searchForm} onSubmit={handleSearchSubmit}>
+        <div>
+          <input
+            type="text"
+            placeholder="DOGE etc."
+            value={searchTerm}
+            onChange={handleSearchChange}
+            className={styles.searchInput}
+          />
+        </div>
+        <div>
+          <button type="submit" className="search-button">
+            Search
+          </button>
+        </div>
+      </form>
+
+      {/* Показываем запрос в консоли */}
+      {searchQuery && <p>Searching for: {searchQuery}</p>}
+    </div>
+  );
+};
+
+export default Header;
--- src/components/listCoins/ListCoins.tsx
+++ src/components/listCoins/ListCoins.tsx
@@ -5,21 +5,25 @@ const ListCoins = () => {
   return (
     <div className={styles.listCoins}>
       <h2>List of Coins</h2>
+      <button className="update-button">Update list</button>
       <table className={styles.table}>
         <thead>
           <tr>
             <th>Name</th>
             <th>Symbol</th>
             <th>Price</th>
-            <th></th>
+            <th>Refresh</th>
+            <th>Remove</th>
           </tr>
         </thead>
         <tbody>
-          {/* Sample data */}
           <tr>
-            <td>Bitcoin</td>
-            <td>BTC</td>
-            <td>$60,000</td>
+            <td>Dogecoin</td>
+            <td>DOGE</td>
+            <td>$0,18</td>
+            <td>
+              <button>Refresh</button>
+            </td>
             <td>
               <button>Remove</button>
             </td>
